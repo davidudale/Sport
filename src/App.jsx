182b6081121fa4296887637636e2f11b86b0f539
davidudale <davison.any@gmail.com>
2025-10-26 14:12:10 +0100

    Next

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+
 
 function App() {
 
   return (
     <>
-      <div>Welcome Home</div>
+      <Navbar />
+     
     </>
   )
 }
