a5e19b14c240d2b3f15dd981bee0a67ea31f060b
davidudale <davison.any@gmail.com>
2025-10-27 10:08:04 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
+
 
 
 function App() {
 
   return (
     <>
-      <Navbar />
+     <div>App</div> 
      
     </>
   )
---
+++ src/components/Parts/UpperNavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+
+const UpperNavBar = () => {
+  return (
+    <>
+      {/*Upper NavBar */}
+      <div className="bg-primary/40 py-2">
+        <div className="@container flex justify-between items-center">
+          <div>
+            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={Logo} alt="" className="w-10" />
+              AllSports
+            </a>
+          </div>
+          {/*Search Bar */}
+          <div>
+            <div className="group relative hidden sm:block">
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] group-hover:border-primary transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-primary "
+              />
+              <IoMdSearch className="absolute text-gray-500 group-hover:text-primary top-1/2 -translate-y-1/2 right-3" />
+            </div>
+          </div>
+          {/*Order Button */}
+          <button className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center cursor-pointer gap-3 group ">
+            <span className="hidden group-hover:block transition-all duration-200">
+              Order
+            </span>
+            <FaCartShopping className="text-xl text-white drop-shadow-sm " />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UpperNavBar;
